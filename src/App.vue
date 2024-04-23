5d2d73c0b4cf1d90de3d3f1160f1089234f182c0
tctienn <mainichichimvm@gmail.com>
2024-04-23 19:34:42 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- <Header/> -->
+  <!-- <div v-for="value in ay" v-bind:key="value">
+    {{ value }}
+  </div> -->
+
+  <!-- <Todos/> -->
+
+  <router-view />
+</template>
+
+<script>
+// import Header from './components/Header';
+
+// import { ref } from 'vue';
+
+// import Todos from './components/Todos'
+
+export default {
+  name: "app",
+  components: {
+    // Header
+    // Todos
+  },
+};
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,12 @@
+
+<template>
+  <div>ass</div>
+</template>
+<script >
+export default {
+  name: "LsdS",
+  setup() {
+    return {};
+  },
+};
+</script>
